97fa9c967ba057ce93a6e52bba587010d228d444
YU SONG <3125511S@student.gla.ac.uk>
2026-03-19 00:37:23 +0000

    test

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Category, Transaction, Budget
 
-# Register your models here so they appear in the Django admin panel.
+
 admin.site.register(Category)
 admin.site.register(Transaction)
 admin.site.register(Budget)
--- tracker/utils.py
+++ tracker/utils.py
@@ -1,22 +1,5 @@
-"""
-工具函数模块
-处理汇率转换等辅助功能
-"""
-
-
 def convert_currency(amount, from_currency, to_currency, exchange_rates=None):
-    """
-    汇率转换函数
-    
-    Args:
-        amount (float): 金额
-        from_currency (str): 源货币代码
-        to_currency (str): 目标货币代码
-        exchange_rates (dict): 汇率字典
-    
-    Returns:
-        float: 转换后的金额
-    """
+ 
     if exchange_rates is None:
         exchange_rates = {
             'USD': 1.0,
